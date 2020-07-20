90f9a3416eeddad5da9b33e0981db6891e2048f2
Calvin Hu <ycalvinhu@gmail.com>
2020-07-19 17:06:46 -0700

    add docker-compose

--- section5/visits/index.js
+++ section5/visits/index.js
@@ -1,19 +1,22 @@
-const express =require('express');
-const redis = require('redis')
+const express = require("express");
+const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: "redis-server", // Comes from the docker-compose.yml. docker-compose will assume this refers to the redis-sever in the docker-compose file
+  port: 6379,
+});
 
-client.set('visits', 0)
+client.set("visits", 0);
 
-app.get('/', (req,res) => {
-  client.get('visits', (err, visits) => {
-    res.send(`Number of visits is ${visits}`)
-    client.set('visits', parseInt(visits) + 1)
-  })
-})
+app.get("/", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send(`Number of visits is ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
 
-const port = 8000
+const port = 8000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
